460d6285482f498a00080169785767b554060ac1
ShyGuyPy <luke.vawter1@gmail.com>
2019-12-19 15:31:15 -0500

    working on a version that can run new features on ES8

--- automate_test_ES10.py
+++ automate_test_ES10.py
@@ -1,9 +1,40 @@
+
+#test model is 'Markov Chain Weather' found in ExtendSim10/Exapmles/Continuous/Standard Block Models
+
 import win32com.client
 import win32gui as wg
 import win32con
 import time
 
+#--------------extend sim menu command codes required format----------------
+#these, and more, can be found on  page 417 of ExendSimTechnicalReferenece.pdf
+run_simulation = """ExecuteMenuCommand(6000)"""
+stop_simulation = """ExecuteMenuCommand(30000)"""
+pause_simulation = """ExecuteMenuCommand(30001)"""
 
+recent_file_1 = """ExecuteMenuCommand(1555)"""
+recent_file_2 = """ExecuteMenuCommand(1556)"""
+recent_file_3 = """ExecuteMenuCommand(1557)"""
+recent_file_4 = """ExecuteMenuCommand(1558)"""
+recent_file_5 = """ExecuteMenuCommand(1559)"""
+
+new_model =  """ExecuteMenuCommand(2)"""
+open_model = """ExecuteMenuCommand(3)"""
+close_model= """ExecuteMenuCommand(4)"""
+
+save_model = """ExecuteMenuCommand(5)"""
+save_model_as = """ExecuteMenuCommand(6)"""
+
+properties = """ExecuteMenuCommand(2001)"""
+exit_or_quit = """ExecuteMenuCommand(1)"""
+
+cut = """ExecuteMenuCommand(18)"""
+copy= """ExecuteMenuCommand(19)"""
+paste = """ExecuteMenuCommand(20)"""
+clear = """ExecuteMenuCommand(21)"""
+#----------------------------------------------------------------------------
+
+#use win32 to open Extend Sim and set window
 def open_and_ID(prog_ID, win_ID):
     program_handle = win32com.client.Dispatch(prog_ID)
     app_ID = wg.FindWindow(None, win_ID)
@@ -14,10 +45,11 @@ def open_and_ID(prog_ID, win_ID):
     print(app_ID)
     return program_handle
 
+#run currently open model(defalt model to open can be set in Extend Sim)
 def run_by_id(prog_ID, win_ID):
     program_handle = win32com.client.Dispatch(prog_ID)
     app_ID = wg.FindWindow(None, win_ID)
-    program_handle.Execute("""ExecuteMenuCommand(6000)""")
+    program_handle.Execute(run_simulation)#"""ExecuteMenuCommand(6000)""")
 
 def set_and_run(prog_ID, win_ID, SetEndTime, SetStartTime, SetNumSim, SetNumStep):
     program_handle = win32com.client.Dispatch(prog_ID)
@@ -26,6 +58,7 @@ def set_and_run(prog_ID, win_ID, SetEndTime, SetStartTime, SetNumSim, SetNumStep
     execute_input = """SetRunParameters({}, {}, {}, {})""".format(SetEndTime, SetStartTime, SetNumSim, SetNumStep)
     program_handle.Execute(execute_input)
 
+#this is just a wrapper for the sleep() function for clarity sake
 def wait_time(x):
     time.sleep(x)
 
@@ -40,7 +73,7 @@ es_handle = open_and_ID("Extend.application", "ExtendSim")
 wait_time(2)
 
 
-#sets run parameters and then runt he model
+#sets run parameters and then run the model
 set_and_run("Extend.application", "ExtendSim", 1000, 0 , 1, 1)
 
 #run open model
---
+++ automation_test_ES8.py
@@ -0,0 +1,2 @@
+#so far Extend Sim 8 seems to work with the automate_test_ES10.py script just fine.
+#further testing needed
